be67729612f673139c93ae988c8558d9b361b1ab
Brad Kuhne <bjkuhne@aol.com>
2021-12-18 10:43:20 -0600

    Added today's date to header

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+// Variable for date at top
+var todayDateEl = document.querySelector('#currentDay');
+// console.log (todayDateEl);
+var todayDate = moment().format('dddd, MMMM DD') ;
+    todayDateEl.textContent = todayDate;
+    // console.log (todayDateEl.textContent);
+
+
+
+
+
